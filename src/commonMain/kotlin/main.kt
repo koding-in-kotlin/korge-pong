fba43d29f62984d892ee97d6fddd0f2e88a69c6e
Igor Mosyagin <408698+cra@users.noreply.github.com>
2022-11-11 12:55:36 +0100

    Add and remove circles by pressing SPACE and BACKSPACE respectively

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -11,10 +11,11 @@ import kotlin.random.*
 suspend fun main() = Korge(title = "Ideal gas here we go", width = 768, height = 768, bgcolor = Colors["#003049"]) {
     val sceneContainer = sceneContainer()
 
-    sceneContainer.changeTo({ MyScene() })
+    sceneContainer.changeTo({ GasBox() })
 }
 
-const val DONT_GO_SLOWER_THAN = 0.5
+const val DONT_GO_SLOWER_THAN = 1.0
+const val SPEED_DELTA = 2
 val MY_COLORS = listOf(
     Colors.HOTPINK,
     Colors.HOTPINK,
@@ -26,32 +27,42 @@ val MY_COLORS = listOf(
 )
 
 class GasParticle(
-    val scene: SContainer,
-    var velocity: Double,
-    input: Input,
+    scene: Scene,
+    var mySpeed: Double,
 ) {
+    val sc = scene.sceneContainer
+    var direction = 1
 
-    val circle = this.scene.circle(Random.nextDouble(5.0, 25.0), stroke = Colors["#FCBF49"], strokeThickness = 3.0, fill = Colors.PINK) {}
+    val circle = sc.circle(Random.nextDouble(5.0, 25.0), stroke = Colors["#FCBF49"], strokeThickness = 3.0, fill = MY_COLORS.random()) {}
 
+    fun die() {
+        sc.removeChild(circle)
+    }
     init {
-        circle.x = Random.nextDouble(circle.radius, this.scene.width - circle.radius)
-        circle.y = Random.nextDouble(circle.radius, this.scene.height - circle.radius)
-        registerUpdaters(input, this.scene.height)
+        circle.x = Random.nextDouble(circle.radius, sc.width - circle.radius)
+        circle.y = Random.nextDouble(circle.radius, sc.height - circle.radius)
+        registerUpdaters(scene.input, sc.height)
     }
 
     fun registerUpdaters(input: Input, maxHeight: Double, addMouse: Boolean = false) {
-        speedUpdater(input)
+        InputHandler(input)
         boundaryUpdater(maxHeight)
         if (addMouse) mouseXUpdater(input)
     }
 
     private fun boundaryUpdater(maxHeight: Double) {
         circle.addFixedUpdater(60.timesPerSecond) {
-            y += velocity
-            if (
-                ((y + 2 * radius) > maxHeight) || y < 0
-            ) {
-                velocity = -velocity
+            y += mySpeed * direction
+            if ((y + 2 * radius) > maxHeight) {
+                y = maxHeight - 2*radius
+                direction = -direction
+                var newColor = MY_COLORS.random()
+                while (newColor == this.color)
+                    newColor = MY_COLORS.random()
+                this.color = newColor
+            } else if (y < 0) {
+                y = 2*radius
+                direction = -direction
                 var newColor = MY_COLORS.random()
                 while (newColor == this.color)
                     newColor = MY_COLORS.random()
@@ -67,37 +78,51 @@ class GasParticle(
         }
     }
 
-    private fun speedUpdater(input: Input) {
+    private fun InputHandler(input: Input) {
         circle.addUpdater {
-            var dvy = 5.0
-            if (input.keys.justPressed(Key.UP)) {
-                if (velocity < 0)
-                    dvy = -dvy
-                if (kotlin.math.abs(velocity + dvy) > DONT_GO_SLOWER_THAN)
-                    velocity += dvy
-            }
-            if (input.keys.justPressed(Key.DOWN)) {
-                if (velocity > 0)
-                    dvy = -dvy
-                if (kotlin.math.abs(velocity + dvy) > DONT_GO_SLOWER_THAN)
-                    velocity += dvy
+            if (input.keys.justPressed(Key.UP))
+                mySpeed += SPEED_DELTA
+            if (input.keys.justPressed(Key.DOWN))
+                mySpeed -= SPEED_DELTA
+
+            if (mySpeed < DONT_GO_SLOWER_THAN) {
+                println("YOU'RE GOING TOO SLOW ${mySpeed}!!! STAPH")
+                mySpeed = DONT_GO_SLOWER_THAN
             }
             if (input.keys.justReleased(Key.ENTER))
-                Console.info("circle coords x=$x y=$y v=${velocity} c=${circle.color}")
+                Console.info("circle coords x=$x y=$y speed=${mySpeed} c=${circle.color}")
         }
     }
 }
 
-class MyScene : Scene() {
+class GasBox : Scene() {
     override suspend fun SContainer.sceneInit() {
         println("Hello there, humans!")
         println(listOf(Colors.PINK, Colors.AQUA, Colors.DARKRED, Colors.DARKKHAKI)[0])
     }
 
+    val particles = arrayListOf<GasParticle>()
+
     // TODO: make something nice
     override suspend fun SContainer.sceneMain() {
-        (DONT_GO_SLOWER_THAN.toInt()..100).forEach{
-            GasParticle(this, it.toDouble(), input)
+        // maybe doublerange here?
+        (DONT_GO_SLOWER_THAN.toInt()..3).forEach {
+            println("YO $it, ${it.toDouble()}")
+            particles.add(GasParticle(this@GasBox, it.toDouble()))
+        }
+        // maybe extract it
+        this.addUpdater {
+            if (input.keys.justPressed(Key.SPACE))
+                particles.add(
+                    GasParticle(
+                        this@GasBox,
+                        Random.nextDouble(DONT_GO_SLOWER_THAN, 30.0),
+                    )
+                )
+            if (input.keys.justPressed(Key.BACKSPACE)) {
+                particles.removeLastOrNull()?.die()
+            }
+
         }
     }
 }
--- src/todo.md
+++ src/todo.md
@@ -1,29 +1,33 @@
 # Super plan
 
-## First session
+## First session: first steps
 
 [x] cloned the project
 [x] made a circle that bounces
 
-## Second session
+## Second session: single jumpy ball
 
 [x] change something on border collision
 [x] change velocity when pressing keyboard buttons
 [x] store velocity as a individual property of the circle
 
-## Third session
+## Third session: multiple jumpy balls
 
 [x] instantiate circles properly
 [~] Ideal Gas route
     [x] add multiple circles
     [x] have a way to store velocities of circles of different colors
 
+## Four session: adding and removing ~balls~ particles on demand
+
+[x] bugfix directions
+[x] have a keypress that adds a particle to the box
+[x] have a keypress that deletes a particle from the box
+
 
 ## Brave New World
 
-[ ] have a text with velocity inside the scene
-[ ] have a keypress that adds a circle to the box
-
+[ ] have a text with velocity inside the scene (some sort of UI)
 [ ] Ideal Gas route
     [ ] start colliding
     [ ] check dataclasses
