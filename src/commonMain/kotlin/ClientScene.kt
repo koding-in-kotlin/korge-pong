a97377a83bbdefac57bd841caf68e1b3efcb4906
asm0dey <pavel.finkelshtein@gmail.com>
2023-03-03 12:06:05 +0100

    WE STARTED AN UI

--- src/commonMain/kotlin/ClientScene.kt
+++ src/commonMain/kotlin/ClientScene.kt
@@ -1,19 +1,27 @@
+import com.soywiz.korge.input.*
 import com.soywiz.korge.scene.*
+import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
-import com.soywiz.korim.color.*
-import com.soywiz.korim.text.*
 import com.soywiz.korio.net.*
 import com.soywiz.korma.geom.*
-import kotlin.random.*
 
 class ClientScene : Scene() {
     override suspend fun SContainer.sceneInit() {
-        val state = GameState(
-            120.2, 150.0, Point(30.0, 50.0), Vector2D(5.0, 5.0)
-        )
-        val client = createTcpClient()
-        client.connect("127.0.0.1", 5050)
-        client.write(state.toMessage())
+
+        uiVerticalStack(width = 150.0) {
+            uiButton("Open Window List") {
+                onClick {
+                    val state = GameState(
+                        120.2, 150.0, Point(30.0, 50.0), Vector2D(5.0, 5.0)
+                    )
+                    val client = createTcpClient("127.0.0.1", 5050)
+//                    client.connect()
+                    client.write(state.toMessage())
+
+                }
+            }
+        }.position(32.0, 32.0)
+
     }
 
 }
